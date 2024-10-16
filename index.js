4ec1b74689c9c47e5bc1cd5396580ab41cca688e
King <133107592+BlackFoxWeb@users.noreply.github.com>
2024-10-16 23:12:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+const neonText = document.getElementById('neonText');
+
+// Если хочешь добавить какие-то эффекты или анимации при движении курсора, можно добавить этот скрипт
+document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    neonText.style.transform = `translate(${x / 100}px, ${y / 100}px)`;
+});
